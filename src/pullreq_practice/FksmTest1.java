e2dcd383f13a79dfa2ce731a03bc662d630c4425
GitHiroki <ringo_hiro@hotmail.co.jp>
2019-11-25 22:59:51 +0900

    FksmTest1 commit

---
+++ src/pullreq_practice/FksmTest1.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package pullreq_practice;
+
+/**
+ * @author HIROKI
+ *
+ */
+public class FksmTest1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello fksm!");
+
+	}
+
+}
